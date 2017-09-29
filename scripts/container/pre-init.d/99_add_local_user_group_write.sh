c6077158b8401a4ba62baa55089a69cf339d240d
Jacob Sanford <jsanford@unb.ca>
2017-09-29 11:10:52 -0300

    Remove unneeded references to local from parent container

--- scripts/container/pre-init.d/99_add_local_user_group_write.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-if [ "$DEPLOY_ENV" == "local" ]; then
-  chgrp -R "${LOCAL_USER_GROUP}" "${SYMFONY_ROOT_DIR}/app"
-  chgrp -R "${LOCAL_USER_GROUP}" "${SYMFONY_ROOT_DIR}/src"
-  chmod g+w -R "${SYMFONY_ROOT_DIR}/app"
-  chmod g+w -R "${SYMFONY_ROOT_DIR}/src"
-fi
